e214ada9a660c3d7e1e9bcce7645d2ad5d018d4a
daviddelacalle <davidelacalle@hotmail.com>
2020-09-24 17:27:12 +0200

    code cleaned of comments

--- src/app/services/drivers.service.ts
+++ src/app/services/drivers.service.ts
@@ -1,94 +1,10 @@
-import { mapToMapExpression } from '@angular/compiler/src/render3/util';
-import { newArray } from '@angular/compiler/src/util';
 import { Injectable } from '@angular/core';
 import * as data from "./../../assets/json/drivers_karts.json";
-import { race } from 'rxjs';
 
 @Injectable()
 export class DriversService {
 
-    private drivers: any  = (data  as  any).default;;
-      
-    // private race0: any[];
-    // private race1: any[];
-    // private race2: any[];
-    // private race3: any[];
-    // private race4: any[];
-    // private race5: any[];
-    // private race6: any[];
-    // private race7: any[];
-    // private race8: any[];
-    // private race9: any[];
-
-
-
-    // initializeRaces(){
-    //     let i = 0;
-    //     let j = 0;
-    //     this.race0 = [];
-    //     this.race1 = [];
-    //     this.race2 = [];
-    //     this.race3 = [];
-    //     this.race4 = [];
-    //     this.race5 = [];
-    //     this.race6 = [];
-    //     this.race7 = [];
-    //     this.race8 = [];
-    //     this.race9 = [];
-
-    //     for(let driver of this.drivers){
-    //         this.race0[i] = [driver.name, driver.races[0].time];
-    //         this.race1[i] = [driver.name, driver.races[1].time];
-    //         this.race2[i] = [driver.name, driver.races[2].time];
-    //         this.race3[i] = [driver.name, driver.races[3].time];
-    //         this.race4[i] = [driver.name, driver.races[4].time];    
-    //         this.race5[i] = [driver.name, driver.races[5].time];
-    //         this.race6[i] = [driver.name, driver.races[6].time];
-    //         this.race7[i] = [driver.name, driver.races[7].time];
-    //         this.race8[i] = [driver.name, driver.races[8].time];
-    //         this.race9[i] = [driver.name, driver.races[9].time];
-    //         i++;
-    //     }
-    //     this.races = [this.race0, this.race1, this.race2, this.race3, this.race4, this.race5, this.race6, this.race7, this.race8, this.race9];
-    // }
-
-    // initializeRaces(){
-    //     this.raceX = new Array();
-    //     for(let driver of this.drivers){
-    //         //Navegamos por los conductores
-    //         for(let race of driver.races){
-    //             //Navegamos por las carreras de cada conductor
-    //             //this.raceX[race.name].push([driver.name, race.time]);
-    //             let finded: boolean = false;
-    //             //console.log(this.raceX.length);
-    //            for(let i = 0; i<this.raceX.length; i++){
-    //                 //Navegamos por las carreras guardadas
-    //                 if(this.raceX[i][0] != null){
-    //                     if(race.name == this.raceX[i][0]){
-    //                         this.raceX[i].push([driver.name, race.time]);
-    //                         finded = true;
-    //                     }
-    //                 }
-    //             }
-    //             if(!finded){
-    //                 this.raceX.push([race.name]);
-    //                 this.raceX[this.raceX.length-1].push([driver.name, race.time]);
-    //                 console.log(this.raceX);
-    //             }
-    //         }
-    //     }
-
-    //     console.log(this.raceX.length);
-    //     console.log(this.raceX[0][0]);
-    //     console.log(this.raceX[1][0]);
-    //     console.log(this.raceX[2][0]);
-    //     console.log(this.raceX[0][22]);
-    //     //console.log(this.raceX[1][0][1]);
-    //     this.orderRaces(this.raceX);
-    // }
-
-    private races: any[][];
-    private raceX: any[];
+    private drivers: any  = (data  as  any).default;
     private raceMap = new Map();
     private ganadores = new Map();
 
@@ -102,8 +18,10 @@ export class DriversService {
 
     }
 
-    //funcion para ordenar el mapa respecto al tiempo en segundos de su carrera
+    
+    //Funcion para ordenar el mapa respecto al tiempo en segundos de su carrera
     //No se puede hacer simplemente un sort ya que ordenaría alfabeticamente los nombres
+    
     sortTimesRaces(a, b) {
         //comparo los diferentes valores de tiempo para ordenarlos
         if (a[3] === b[3]) {
@@ -157,7 +75,6 @@ export class DriversService {
         }
         let m2= new Map([...this.ganadores.entries()].sort((a,b) => b[1] - a[1]));
         this.ganadores = m2;
-
     }
 
     calculatePoints(race){
